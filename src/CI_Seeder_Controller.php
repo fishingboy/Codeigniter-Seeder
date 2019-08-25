d4d454beebf79d0ef953eb4624d8ceb5af585deb
Leo.Kuo <et282523@hotmail.com>
2019-08-26 00:34:17 +0800

    support codeigniter 2

--- src/CI_Seeder_Controller.php
+++ src/CI_Seeder_Controller.php
@@ -24,10 +24,10 @@ class CI_Seeder_Controller extends \CI_Controller
         parent::__construct();
 
         // // 判斷換行字元
-        $this->_nl = (is_cli()) ? "\n" : "<br>";
+        $this->_nl = ($this->input->is_cli_request()) ? "\n" : "<br>";
 
         // 限定本機或 Command Line 使用
-        if ( ! is_cli() &&
+        if ( ! $this->input->is_cli_request() &&
             $_SERVER['REMOTE_ADDR'] != '127.0.0.1' &&
             $_SERVER['REMOTE_ADDR'] != '::1' &&
             false === strpos($_SERVER['REMOTE_ADDR'], '192.168.')
